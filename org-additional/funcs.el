ecb5eba02020f8493dd1ad54a55f291095315a44
chriad <chriad.mueller@protonmail.com>
2021-10-13 13:30:11 +0200

    funcs for org-additional

---
+++ org-additional/funcs.el
@@ -0,0 +1,35 @@
+(defun org-hide-properties ()
+  "Hide all org-mode headline property drawers in buffer. Could be slow if it has a lot of overlays."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward
+            "^ *:properties:\n\\( *:.+?:.*\n\\)+ *:end:\n" nil t)
+      (let ((ov_this (make-overlay (match-beginning 0) (match-end 0))))
+        (overlay-put ov_this 'display "")
+        (overlay-put ov_this 'hidden-prop-drawer t))))
+  (put 'org-toggle-properties-hide-state 'state 'hidden))
+
+(defun org-show-properties ()
+  "Show all org-mode property drawers hidden by org-hide-properties."
+  (interactive)
+  (remove-overlays (point-min) (point-max) 'hidden-prop-drawer t)
+  (put 'org-toggle-properties-hide-state 'state 'shown))
+
+(defun org-toggle-properties ()
+  "Toggle visibility of property drawers."
+  (interactive)
+  (if (eq (get 'org-toggle-properties-hide-state 'state) 'hidden)
+      (org-show-properties)
+    (org-hide-properties)))
+
+
+
+
+(defun org-hide-src-block-delimiters ()
+  (interactive)
+  (save-excursion (goto-char (point-max))
+                  (while (re-search-backward "#\\+BEGIN_SRC\\|#\\+END_SRC" nil t)
+                    (let ((ov (make-overlay (line-beginning-position)
+                                            (1+ (line-end-position)))))
+                      (overlay-put ov 'invisible t)))))
